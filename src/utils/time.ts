104c71d6d39871f00bdcae28ad34b449790dfa89
Noah Saso <noahsaso@gmail.com>
2024-02-16 15:22:18 -0800

    snap range bounds to keep the dates clean

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -12,7 +12,6 @@ export enum TimeRange {
 export const isValidTimeRange = (range: string): range is TimeRange =>
   Object.values(TimeRange).includes(range as TimeRange)
 
-
 // The duration in seconds of a given range.
 const rangeDuration: Record<TimeRange, number> = {
   [TimeRange.Year]: 365 * 24 * 60 * 60,
@@ -23,10 +22,25 @@ const rangeDuration: Record<TimeRange, number> = {
 }
 
 /**
- * Get the bounds of a given range.
+ * Get the bounds in seconds of a given range.
  */
 export const getRangeBounds = (range: TimeRange) => {
-  const end = Math.floor(Date.now() / 1000)
+  const endDate = new Date()
+  // Snap to a reasonable point in time.
+  switch (range) {
+    case TimeRange.Hour:
+      endDate.setSeconds(0, 0)
+      break
+    case TimeRange.Day:
+    case TimeRange.Week:
+      endDate.setMinutes(0, 0, 0)
+      break
+    default:
+      endDate.setHours(0, 0, 0)
+  }
+
+  // Floor is redundant since snapping above should clear milliseconds.
+  const end = Math.floor(endDate.getTime() / 1000)
   const start = end - rangeDuration[range]
 
   return {
