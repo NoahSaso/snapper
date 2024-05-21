bef88df55e8ea3698f8fd7d789df7a712c947ea8
Lena <elena.mikhaylova0@gmail.com>
2024-05-21 09:49:35 -0400

    change to demo api

--- src/query/queries/coingecko.ts
+++ src/query/queries/coingecko.ts
@@ -12,9 +12,9 @@ export const coingeckoPriceQuery: Query<
   name: 'coingecko-price',
   parameters: ['id'],
   url: ({ id }) =>
-    `https://pro-api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=usd`,
+    `https://api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=usd`,
   headers: {
-    'x-cg-pro-api-key': COINGECKO_API_KEY,
+    'x-cg-demo-api-key': COINGECKO_API_KEY,
   },
   transform: (body, { id }) => (body[id] as { usd: number }).usd,
   // Cache price for 5 minutes.
@@ -45,10 +45,10 @@ export const coingeckoPriceHistoryQuery: Query<
       endTime ? new Date(Number(endTime)) : undefined
     )
 
-    return `https://pro-api.coingecko.com/api/v3/coins/${id}/market_chart/range?vs_currency=usd&from=${BigInt(start).toString()}&to=${BigInt(end).toString()}`
+    return `https://api.coingecko.com/api/v3/coins/${id}/market_chart/range?vs_currency=usd&from=${BigInt(start).toString()}&to=${BigInt(end).toString()}`
   },
   headers: {
-    'x-cg-pro-api-key': COINGECKO_API_KEY,
+    'x-cg-demo-api-key': COINGECKO_API_KEY,
   },
   transform: (body) => body.prices as [number, number][],
   // Cache for:
